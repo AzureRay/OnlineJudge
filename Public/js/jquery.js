922c70aa8b5ea2872a8c004e5c1fe7d563bd8a6a
calmound <245708505@qq.com>
2015-10-13 14:44:21 +0800

    modify header index footer

--- Public/js/jquery.js
+++ Public/js/jquery.js
@@ -1,13 +1,15 @@
-    $(function(){
-        $('.hidden_nav').hover(function(){
-            if($('.hidden_nav > ul').is(':hidden')){
-                $('.hidden_nav > ul').css('display','block');
-            }
-            return false;
-        },function(){
-            if($('.hidden_nav > ul').is(':visible')){
+$(function(){
+    $('.hidden_nav').hover(function(){
+        if($('.hidden_nav > ul').is(':hidden')){
+            $('.hidden_nav > ul').css('display','block');
+        }
+        return false;
+    },function(){
+        if($('.hidden_nav > ul').is(':visible')){
+            setTimeout(function(){
                 $('.hidden_nav > ul').css('display','none');
-            }
-            return false;
-        });
+            },200);
+        }
+        return false;
     });
+});
